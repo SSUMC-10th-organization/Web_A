0faa4ad755bff84a98b480ed58018c9e0e536535
bum22 <jdtgj1@soongsil.ac.kr>
2026-04-05 20:02:19 +0900

    파일 업로드

---
+++ doldol/Week3/week3/src/main.tsx
@@ -0,0 +1,14 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
+
+// 수정 후 (더 안전한 방식)
+const container = document.getElementById("root");
+if (container) {
+	createRoot(container).render(
+		<StrictMode>
+			<App />
+		</StrictMode>,
+	);
+}
