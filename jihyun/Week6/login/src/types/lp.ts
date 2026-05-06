9b61f1273aa34689fb1a8248e13f09db86df10d8
yjudy0531 <yjudy0531@gmail.com>
2026-05-06 23:01:09 +0900

    [week6/mission] 무한스크롤 부분 useInfiniteQuery로 무한스크롤 구현해보기 + 스켈레톤 UI

---
+++ jihyun/Week6/login/src/types/lp.ts
@@ -0,0 +1,73 @@
+export type Tag = {
+  id: number;
+  name: string;
+};
+
+export type Like = {
+  id: number;
+  userId: number;
+  lpId: number;
+};
+
+export type LP = {
+  id: number;
+  title: string;
+  content: string;
+  thumbnail: string;
+  published: boolean;
+  authorId: number;
+  createdAt: string;
+  updatedAt: string;
+  deletedAt: string | null;
+  tags: Tag[];
+  likes: Like[];
+};
+
+export type CommonResponse<T> = {
+  status: boolean;
+  statusCode: number;
+  message: string;
+  data: T;
+};
+
+export type CursorBasedResponse<T> = {
+  data: T;
+  nextCursor: number | null;
+  hasNext: boolean;
+};
+
+export type GetLPListParams = {
+  cursor?: number;
+  limit?: number;
+  search?: string;
+  order?: 'asc' | 'desc';
+};
+export type Comment = {
+  id: number;
+  content: string;
+  lpId: number;
+  authorId: number;
+  createdAt: string;
+  updatedAt: string;
+  author: {
+    id: number;
+    name: string;
+    email: string;
+    bio: string | null;
+    avatar: string | null;
+    createdAt: string;
+    updatedAt: string;
+  };
+};
+
+export type GetCommentListParams = {
+  cursor?: number;
+  limit?: number;
+  order?: 'asc' | 'desc';
+};
+
+export type GetCommentListResponse = CommonResponse<CursorBasedResponse<Comment[]>>;
+export type GetLPListResponse = CommonResponse<CursorBasedResponse<LP[]>>;
+
+export type GetLPDetailResponse = CommonResponse<LP>;
+export type InfiniteLPListResponse = CursorBasedResponse<LP[]>;
