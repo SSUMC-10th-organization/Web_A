20727e54c82c9ad7fc404430a0038f85e25b2106
KimEunHye <rladmsgp031219@naver.com>
2026-06-21 16:55:13 +0900

    [week10/chore] 사용X import 제거

--- rene/Week8/Debouncing&Throttling/src/pages/LPDetailPage.tsx
+++ rene/Week8/Debouncing&Throttling/src/pages/LPDetailPage.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useState } from "react";
+import { useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { defaultProfile, backIcon, editIcon, trashIcon } from "../assets";
 import LoadingSpinner from "../components/common/LoadingSpinner";
